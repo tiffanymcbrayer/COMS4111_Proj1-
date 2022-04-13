ca826ba4e50c949124168b84204e26bb2bf46a10
Tiffany McBrayer <tiffanymcbrayer@tiffanys-mbp.myfiosgateway.com>
2022-04-12 22:33:14 -0400

    added

--- webserver/server.py
+++ webserver/server.py
@@ -142,7 +142,7 @@ def another():
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
-  name = request.form['name']
+  name = request.form['user']
   print(name)
   cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
   g.conn.execute(text(cmd), name1 = name, name2 = name);
