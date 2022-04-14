b8eac187981e92e8e8127bca79d439255d55b03f
Tiffany McBrayer <tiffanymcbrayer@Tiffanys-MacBook-Pro.local>
2022-04-14 19:37:31 -0400

    deleted

--- webserver/server.py
+++ webserver/server.py
@@ -34,13 +34,6 @@ DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2
 engine = create_engine(DATABASEURI)
 
 
-# Here we create a test table and insert some values in it
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 
 
 userID_ = -1
