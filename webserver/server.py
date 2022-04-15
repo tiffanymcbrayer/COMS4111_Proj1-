87da99ea4fa98afd9b1202031f3e43aee899b2f5
Tiffany McBrayer <tiffanymcbrayer@Tiffanys-MacBook-Pro.local>
2022-04-14 20:04:02 -0400

    added

--- webserver/server.py
+++ webserver/server.py
@@ -151,8 +151,6 @@ def add():
   print(name, waitTime, cover, minSpend, capacity, ageMin, ageMax, group)
 
 
-  # cmd = 'INSERT INTO users VALUES (:user1)'
-  # g.conn.execute(text(cmd), user1 = user)
   return redirect('/')
 
 
