cdc6db8814a5e8edd4545c61d26e677d9fa33eb7
Tiffany McBrayer <tiffanymcbrayer@tiffanys-mbp.myfiosgateway.com>
2022-04-15 07:04:25 -0400

    event

--- webserver/server.py
+++ webserver/server.py
@@ -138,7 +138,7 @@ def events(id = None):
 
   for eventid, placeid, placename, name, description, numberattendees, address, date, recur, allday, timerange, starttime, endtime in eventInfo:
     entry = [placename, name, description, numberattendees, address, date, recur, allday, timerange, starttime, endtime, eventid]
-    print(eventID)
+    print(eventid)
   eventDict = dict(event = entry)
   eventInfo.close()
 
