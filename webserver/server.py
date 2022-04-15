aa90af4fa492504e8ec541401fe50d913d9ede89
Tiffany McBrayer <tiffanymcbrayer@tiffanys-mbp.myfiosgateway.com>
2022-04-14 21:46:20 -0400

    add

--- webserver/server.py
+++ webserver/server.py
@@ -147,9 +147,8 @@ def add():
     users.append(userID[0])
   print(users)
   if int(user) not in users:
-    print("not in db")
-    # cmd = 'INSERT INTO users VALUES (:user1)'
-    # g.conn.execute(text(cmd), user1 = user)
+    cmd = 'INSERT INTO users VALUES (:user1)'
+    g.conn.execute(text(cmd), user1 = user)
 
 
   today = date.today()
