946a1e2eca4648a45bb28d601a93d799be6a0ab9
Tiffany McBrayer <tiffanymcbrayer@tiffanys-mbp.myfiosgateway.com>
2022-04-15 08:20:26 -0400

    e

--- webserver/server.py
+++ webserver/server.py
@@ -235,6 +235,7 @@ def view_name(id = None):
     on temp.placeid = has.placeid and temp.placeID = (:id1)
   """
   eventInfo = g.conn.execute(text(cmd2), id1 = id)
+  print(eventInfo)
   eventList = []
   for result in eventInfo:
     eventList.append([result[0],result[2]]) # just appending the name, will create the hyperlink after
